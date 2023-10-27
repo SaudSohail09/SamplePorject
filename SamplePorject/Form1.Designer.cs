74a9b00de6802a80b52c3d1149e2728ad38ff09a
sauds <sauds@DESKTOP-0THV73E>
2023-10-27 19:09:00 +0500

    Add project files.

---
+++ SamplePorject/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace SamplePorject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.textBoxLName = new System.Windows.Forms.TextBox();
+            this.textBoxFName = new System.Windows.Forms.TextBox();
+            this.PicEmp = new System.Windows.Forms.PictureBox();
+            this.Id = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonShow = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PicEmp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(270, 77);
+            this.textBoxId.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(148, 26);
+            this.textBoxId.TabIndex = 0;
+            // 
+            // textBoxLName
+            // 
+            this.textBoxLName.Location = new System.Drawing.Point(270, 243);
+            this.textBoxLName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxLName.Name = "textBoxLName";
+            this.textBoxLName.Size = new System.Drawing.Size(148, 26);
+            this.textBoxLName.TabIndex = 1;
+            // 
+            // textBoxFName
+            // 
+            this.textBoxFName.Location = new System.Drawing.Point(270, 154);
+            this.textBoxFName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxFName.Name = "textBoxFName";
+            this.textBoxFName.Size = new System.Drawing.Size(148, 26);
+            this.textBoxFName.TabIndex = 2;
+            // 
+            // PicEmp
+            // 
+            this.PicEmp.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PicEmp.Location = new System.Drawing.Point(539, 77);
+            this.PicEmp.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.PicEmp.Name = "PicEmp";
+            this.PicEmp.Size = new System.Drawing.Size(210, 193);
+            this.PicEmp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PicEmp.TabIndex = 3;
+            this.PicEmp.TabStop = false;
+            // 
+            // Id
+            // 
+            this.Id.AutoSize = true;
+            this.Id.Location = new System.Drawing.Point(154, 77);
+            this.Id.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Id.Name = "Id";
+            this.Id.Size = new System.Drawing.Size(23, 20);
+            this.Id.TabIndex = 4;
+            this.Id.Text = "Id";
+            this.Id.Click += new System.EventHandler(this.Id_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 157);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "FirstName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(154, 246);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "LastName";
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.buttonBrowse.BackgroundImage = global::SamplePorject.Properties.Resources.R;
+            this.buttonBrowse.Location = new System.Drawing.Point(656, 337);
+            this.buttonBrowse.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(93, 35);
+            this.buttonBrowse.TabIndex = 7;
+            this.buttonBrowse.Text = "Browse";
+            this.buttonBrowse.UseVisualStyleBackColor = false;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.buttonSave.BackgroundImage = global::SamplePorject.Properties.Resources.R;
+            this.buttonSave.Location = new System.Drawing.Point(149, 337);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(102, 35);
+            this.buttonSave.TabIndex = 8;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = false;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonShow
+            // 
+            this.buttonShow.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.buttonShow.BackgroundImage = global::SamplePorject.Properties.Resources.R;
+            this.buttonShow.Location = new System.Drawing.Point(528, 337);
+            this.buttonShow.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonShow.Name = "buttonShow";
+            this.buttonShow.Size = new System.Drawing.Size(90, 35);
+            this.buttonShow.TabIndex = 9;
+            this.buttonShow.Text = "Show";
+            this.buttonShow.UseVisualStyleBackColor = false;
+            this.buttonShow.Click += new System.EventHandler(this.buttonShow_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.buttonUpdate.BackgroundImage = global::SamplePorject.Properties.Resources.R;
+            this.buttonUpdate.Location = new System.Drawing.Point(291, 337);
+            this.buttonUpdate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(94, 35);
+            this.buttonUpdate.TabIndex = 10;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = false;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.buttonDelete.BackgroundImage = global::SamplePorject.Properties.Resources.R;
+            this.buttonDelete.Location = new System.Drawing.Point(408, 337);
+            this.buttonDelete.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(94, 35);
+            this.buttonDelete.TabIndex = 11;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImage = global::SamplePorject.Properties.Resources.R;
+            this.ClientSize = new System.Drawing.Size(831, 431);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonShow);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Id);
+            this.Controls.Add(this.PicEmp);
+            this.Controls.Add(this.textBoxFName);
+            this.Controls.Add(this.textBoxLName);
+            this.Controls.Add(this.textBoxId);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PicEmp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.TextBox textBoxLName;
+        private System.Windows.Forms.TextBox textBoxFName;
+        private System.Windows.Forms.PictureBox PicEmp;
+        private System.Windows.Forms.Label Id;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonShow;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonDelete;
+    }
+}
+
---
+++ SamplePorject/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace SamplePorject
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\sauds\source\repos\SamplePorject\SamplePorject\ImagesDB.mdf;Integrated Security=True");
+        SqlCommand command;
+        string imgloc = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Id_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            try {
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.Filter = "JPG Files (*.jpg)|*.jpg| GIF Files (*.gif)|*.gif| ALL Files (*.*)|*.*";
+                dlg.Title = "Select Employees Pic";
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    imgloc = dlg.FileName.ToString();
+                    PicEmp.ImageLocation = imgloc;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] img = null;
+                FileStream fs = new FileStream(imgloc, FileMode.Open, FileAccess.Read);
+                BinaryReader br = new BinaryReader(fs);
+                img = br.ReadBytes((int)fs.Length);
+                string sql = "INSERT INTO Employees(Id,Name,Number,Pic) VALUES(" + textBoxId.Text + ",'" + textBoxFName.Text + "','" + textBoxLName.Text + "',@img)";
+                if (conn.State != ConnectionState.Open)
+                    conn.Open();
+                command = new SqlCommand(sql, conn);
+                command.Parameters.Add(new SqlParameter("@img", img));
+                int x = command.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show(x.ToString() + "record(s) saved.");
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            try {
+                string sql = "SELECT Name, Number, Pic From Employees WHERE Id=" + textBoxId.Text + "";
+                if (conn.State != ConnectionState.Open)
+                    conn.Open();
+                command = new SqlCommand(sql, conn);
+                SqlDataReader reader = command.ExecuteReader();
+                reader.Read();
+                if (reader.HasRows)
+                {
+                    textBoxFName.Text = reader[0].ToString();
+                    textBoxLName.Text = reader[1].ToString();
+                    byte[] img = (byte[])(reader[2]);
+                    if(img == null)
+                    {
+                        PicEmp.Image = null;
+                    }
+                    else
+                    {
+                        MemoryStream ms = new MemoryStream(img);
+                        PicEmp.Image = Image.FromStream(ms);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("This id not exist");
+                }
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            try {
+                byte[] img = null;
+                FileStream fs = new FileStream(imgloc, FileMode.Open, FileAccess.Read);
+                BinaryReader br = new BinaryReader(fs);
+                img = br.ReadBytes((int)fs.Length);
+                string sql = "UPDATE Employees SET Name = @Name, Number = @Number, Pic = @Pic WHERE  Id=" + textBoxId.Text + "";
+
+                if (conn.State != ConnectionState.Open)
+                    conn.Open();
+
+                using (SqlCommand command = new SqlCommand(sql, conn))
+                {
+                    command.Parameters.AddWithValue("@Id", textBoxId.Text);
+                    command.Parameters.AddWithValue("@Name", textBoxFName.Text);
+                    command.Parameters.AddWithValue("@Number", textBoxLName.Text);
+                    if (img != null)
+                    {
+                        command.Parameters.Add(new SqlParameter("@Pic", SqlDbType.VarBinary, img.Length)).Value = img;
+                    }
+                    else
+                    {
+                       
+                        command.Parameters.Add(new SqlParameter("@Pic", SqlDbType.VarBinary)).Value = DBNull.Value;
+                    }
+
+                    int x = command.ExecuteNonQuery();
+                }
+                conn.Close();
+                MessageBox.Show("Record updated.");
+
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sql = "DELETE FROM Employees WHERE Id = @Id";
+
+                if (conn.State != ConnectionState.Open)
+                    conn.Open();
+
+                using (SqlCommand command = new SqlCommand(sql, conn))
+                {
+                    command.Parameters.AddWithValue("@Id", textBoxId.Text);
+
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Record deleted.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Record not found or deleted.");
+                    }
+                }
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
